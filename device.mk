35cb0d6e776fac27ea73ae74e76ea98b6370818a
ohjhas <spliteritoob@gmail.com>
2025-12-07 18:23:40 +0530

    a04: change DEVICE_PATH to a04

--- device.mk
+++ device.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/samsung/a05m
+DEVICE_PATH := device/samsung/a04
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
